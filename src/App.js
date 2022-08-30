a957fb2ed33efb8325bab836544cb20924d77fa5
Leonardo Leite <leoinatel@gmail.com>
2022-08-30 19:05:13 -0300

    Requisito 1.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { Switch } from 'react-router-dom';
+import Album from './pages/Album';
+import Favorites from './pages/favorites';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <div>
+        <p>TrybeTunes</p>
+        <Switch>
+          <Login exact path="/" Component={ Login } />
+          <Search exact path="/search" Component={ Search } />
+          <Album exact path="/album/:id" Component={ Album } />
+          <Favorites exact path="/favorites" Component={ Favorites } />
+          <Profile exact path="/profile" Component={ Profile } />
+          <ProfileEdit exact path="/profile/edit" Component={ ProfileEdit } />
+          <NotFound exact path="*" Component={ NotFound } />
+        </Switch>
+      </div>
+    );
   }
 }
 
